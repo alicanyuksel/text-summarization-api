1771dcb00ff82fda5ee412be5154c5c320c34c5c
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-29 01:15:29 +0300

    Add the model name in config and some improvements

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 database_name = "data.db"
+
+# model_name = "sshleifer/distilbart-xsum-12-1"
+# tokenizer_name = "sshleifer/distilbart-xsum-12-1"
+
+model_name = "facebook/bart-large-cnn"
+tokenizer_name = "facebook/bart-large-cnn"
+
+
--- resources/BartModel.py
+++ resources/BartModel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_cors import CORS, cross_origin
+from config import model_name, tokenizer_name
 
 # ML model
 #from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig
@@ -10,26 +11,20 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class BartModel(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument("origin_text", required=True,
+    parser.add_argument("text", required=True,
                         help="This field cannot be left blank!")
 
     def post(self, id_):
         data = BartModel.parser.parse_args()
 
-        # bart-large-cnn model
-        # tokenizer = BartTokenizer.from_pretrained('distilbart-cnn-12-6')
-        # model = BartForConditionalGeneration.from_pretrained(
-        #     'distilbart-cnn-12-6')
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSeq2SeqLM.from_pretrained(tokenizer_name)
 
-        tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-xsum-12-1")
-        model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-xsum-12-1")
-
-        text_to_summarize = data["origin_text"]
+        text_to_summarize = data["text"]
 
         # Encoding the inputs and passing them to model.generate()
 
-        inputs = tokenizer.batch_encode_plus(
-            [text_to_summarize], return_tensors='pt')
+        inputs = tokenizer.batch_encode_plus([text_to_summarize], return_tensors='pt')
         summary_ids = model.generate(inputs['input_ids'], early_stopping=True)
 
         # get summarized text from BART
