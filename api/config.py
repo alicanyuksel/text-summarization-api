98b8c885bb961347041771cc4c2ff8ab76e50c47
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-30 15:48:38 +0300

    Clean config file

--- api/config.py
+++ api/config.py
@@ -1,8 +1,3 @@
-database_name = "data.db"
-
-# model_name = "sshleifer/distilbart-xsum-12-1"
-# tokenizer_name = "sshleifer/distilbart-xsum-12-1"
-
 model_name = "facebook/bart-large-cnn"
 tokenizer_name = "facebook/bart-large-cnn"
 
